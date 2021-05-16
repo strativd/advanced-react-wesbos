d48086bc71f5aa00806a04680822c24b072fb38a
ztratify <strat.barrett@shopify.com>
2021-05-16 14:47:36 -0700

    Next JS layouts: basic app with styled components

---
+++ sick-fits/frontend/components/Page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from './Header';
+
+export default function Page({ children }) {
+  return (
+    <div>
+      <Header />
+      {children}
+    </div>
+  );
+}
