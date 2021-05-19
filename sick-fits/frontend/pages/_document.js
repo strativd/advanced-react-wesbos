f63b4eece77706b7d01e20d652fc5fa533ac8e03
ztratify <strat.barrett@shopify.com>
2021-05-18 21:18:39 -0700

    server rendered stylesheet using styled comps

--- sick-fits/frontend/pages/_document.js
+++ sick-fits/frontend/pages/_document.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
+  // Let Next render styled components on the server
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage(
+      (App) => (props) => sheet.collectStyles(<App {...props} />)
+    );
+    const styleElement = sheet.getStyleElement();
+    return { ...page, styleElement };
+  }
+
   render() {
     return (
       <Html lang="en-CA">
