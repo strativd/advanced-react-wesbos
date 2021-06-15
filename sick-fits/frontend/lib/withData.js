828c7e9ffe843c71ffa1aba2f94a565293be39f9
ztratify <strat.barrett@shopify.com>
2021-06-15 09:54:28 -0700

    Manage cache with custom fields (not working)

--- sick-fits/frontend/lib/withData.js
+++ sick-fits/frontend/lib/withData.js
@@ -4,7 +4,9 @@ import { onError } from '@apollo/link-error';
 import { getDataFromTree } from '@apollo/client/react/ssr';
 import { createUploadLink } from 'apollo-upload-client';
 import withApollo from 'next-with-apollo';
+
 import { endpoint, prodEndpoint } from '../config';
+import paginationField from './paginationField';
 
 function createClient({ headers, initialState }) {
   return new ApolloClient({
@@ -36,6 +38,7 @@ function createClient({ headers, initialState }) {
         Query: {
           fields: {
             // TODO: We will add this together!
+            // But seems to be borked... Refetching queries instead.
             // allProducts: paginationField(),
           },
         },
