80c08487a3686c057723d891688c277641d78ca1
ztratify <strat.barrett@shopify.com>
2021-06-16 15:32:37 -0700

    CartItem field relationships on User schema

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -9,6 +9,7 @@ import {
 import { User } from "./schemas/User";
 import { Product } from "./schemas/Product";
 import { ProductImage } from "./schemas/ProductImage";
+import { CartItem } from "./schemas/CartItem";
 
 import { insertSeedData } from "./seed-data";
 import { sendPasswordResetEmail } from "./lib/mail";
@@ -61,6 +62,7 @@ export default withAuth(
       User,
       Product,
       ProductImage,
+      CartItem,
     }),
     ui: {
       // Show the UI only for poeple who pass this test
