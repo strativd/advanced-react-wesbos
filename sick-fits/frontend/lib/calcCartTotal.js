ba90763f756cda290c7e7ab0d849739b238b3eb6
ztratify <strat.barrett@shopify.com>
2021-06-16 15:33:41 -0700

    Query cart details for user and render cart items

---
+++ sick-fits/frontend/lib/calcCartTotal.js
@@ -0,0 +1,8 @@
+export default function calcCartTotal(cart) {
+  return cart.reduce((tally, cartItem) => {
+    // products can be deleted, but they could still be in your cart
+    if (!cartItem.product) return tally;
+
+    return tally + cartItem.quantity * cartItem.product.price;
+  }, 0);
+}
